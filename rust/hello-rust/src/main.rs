97dd5312d21d20f1c2891bb350a742c63f205293
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-07-07 20:07:20 +0100

    rust | hello-rust | ferris-says: a small Rust application

--- rust/hello-rust/src/main.rs
+++ rust/hello-rust/src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
-    println!("Hello, world!");
-}
+//// Simple Hello World
+//fn main() {
+//    println!("Hello, world!");
+//}
 
+//// A Small Rust application
+// DOCS: https://www.rust-lang.org/learn/get-started
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(out, width, &mut writer).unwrap();
+}
