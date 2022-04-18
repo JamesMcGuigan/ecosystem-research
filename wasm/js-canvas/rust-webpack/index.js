3ae84993388b74c8fc887ad5d36e8da71ccbbe98
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-04-18 02:36:44 +0100

    WASM | Rust Webpack | return String + rust:window.alert()

--- wasm/js-canvas/rust-webpack/index.js
+++ wasm/js-canvas/rust-webpack/index.js
@@ -2,4 +2,6 @@ import('./pkg').then(wasm => {
     console.log('wasm.answer()',    wasm.answer());
     console.log('wasm.squared(42)', wasm.squared(42));
     console.log('wasm.add(1,2)',    wasm.add(1,2));
+    console.log('wasm.hello_string("Jamie")', wasm.hello_string("Jamie"));
+    wasm.alert();
 });
--- wasm/js-canvas/rust-webpack/src/lib.rs
+++ wasm/js-canvas/rust-webpack/src/lib.rs
@@ -1,5 +1,5 @@
 use wasm_bindgen::prelude::*;
-// use wasm_bindgen::Clamped;
+
 
 #[wasm_bindgen]
 pub fn answer() -> i32 {
@@ -14,4 +14,21 @@ pub fn squared(x: i32) -> i32 {
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     return a + b;
+}
+
+// DOCS: https://rustwasm.github.io/wasm-bindgen/reference/types/string.html
+// requires: npm install text-encoding
+#[wasm_bindgen]
+pub fn hello_string(name: String) -> String {
+    // let output = format!("Hello {name}!"); // IntelliJ Warning: Parameter `name` is never used
+    let output = format!("Hello {}!", name);
+    return output;
+}
+
+// DOCS: https://zyghost.com/guides/intro-to-rust-web/2_web_sys_alert.html
+#[wasm_bindgen]
+pub fn alert() -> Result<(), JsValue> {
+    use web_sys::window;
+    let window = window().unwrap();
+    window.alert_with_message("Hello from WASM!")
 }
