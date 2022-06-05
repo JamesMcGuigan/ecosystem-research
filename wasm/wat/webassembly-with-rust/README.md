ef6bcb04000ae03868f26cbb54af69e1b3f0cc5b
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-06-05 11:55:41 +0100

    webassembly-with-rust | fundamentals | add1.wat + README.md

---
+++ wasm/wat/webassembly-with-rust/README.md
@@ -0,0 +1,5 @@
+# Book: Programming WebAssembly with Rust
+
+This folder follows the code examples from the book by Kevin Hoffman
+- https://learning.oreilly.com/library/view/programming-webassembly-with/9781680506846/
+- https://www2.irb.hr/korisnici/zskoda/hoffmanWasmRust.pdf
---
+++ wasm/wat/webassembly-with-rust/fundamentals/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+
+wat2wasm add1.wat -o add1.wasm
+wat2wasm add2.wat -o add2.wasm
+
+# warning: using `--invoke` with a function that returns values is experimental and may break in the future
+wasmtime add1.wat  --invoke add 42 34  2> /dev/null
+wasmtime add1.wasm --invoke add 42 34  2> /dev/null
+wasmtime add2.wat  --invoke add 42 34  2> /dev/null
+wasmtime add2.wasm --invoke add 42 34  2> /dev/null
