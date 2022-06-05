32d352fd07202bf23e76e82716c93888c3a9f7fb
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-06-05 21:03:55 +0100

    webassembly-with-rust | wasmcheckers | add additional tests to index.js

--- wasm/wat/webassembly-with-rust/wasmcheckers/index.js
+++ wasm/wat/webassembly-with-rust/wasmcheckers/index.js
@@ -23,11 +23,15 @@ fetch('./checkers.wasm')
         instance = results.instance;
         instance.exports.initBoard();
         console.log("At start, turn owner is " + instance.exports.getTurnOwner());
-        instance.exports.move(0, 5, 0, 4); // B instance.exports.move(1, 0, 1, 1); // W instance.exports.move(0, 4, 0, 3); // B
-        instance.exports.move(1, 1, 1, 0); // W instance.exports.move(0, 3, 0, 2); // B instance.exports.move(1, 0, 1, 1); // W instance.exports.move(0, 2, 0, 0); // B - this will get a crown instance.exports.move(1, 1, 1, 0); // W
-
-        // B - move the crowned piece out
-        let res = instance.exports.move(0, 0, 0, 2);
+        instance.exports.move(0, 5, 0, 4); // B
+        instance.exports.move(1, 0, 1, 1); // W
+        instance.exports.move(0, 4, 0, 3); // B
+        instance.exports.move(1, 1, 1, 0); // W
+        instance.exports.move(0, 3, 0, 2); // B
+        instance.exports.move(1, 0, 1, 1); // W
+        instance.exports.move(0, 2, 0, 0); // B - this will get a crown
+        instance.exports.move(1, 1, 1, 0); // W
+        let res = instance.exports.move(0, 0, 0, 2); // B - move the crowned piece out
         document.getElementById("container").innerText = res;
         console.log("At end, turn owner is " + instance.exports.getTurnOwner());
     })
