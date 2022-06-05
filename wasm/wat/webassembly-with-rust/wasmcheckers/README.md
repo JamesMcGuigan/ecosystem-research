4be0f7a5617ee69db2d88c56a845f759423ce04a
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-06-05 14:24:52 +0100

    webassembly-with-rust | wasmcheckers | initial files and build.sh

---
+++ wasm/wat/webassembly-with-rust/wasmcheckers/README.md
@@ -0,0 +1,10 @@
+# Programming WebAssembly with Rust - WASM Checkers
+
+This folder follows the code examples from Part 1 Chaper 2 of the book by Kevin Hoffman
+- https://learning.oreilly.com/library/view/programming-webassembly-with/9781680506846/
+- https://www2.irb.hr/korisnici/zskoda/hoffmanWasmRust.pdf
+
+````
+./build.sh
+wasmtime       checkers.wasm
+````
---
+++ wasm/wat/webassembly-with-rust/wasmcheckers/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd $(dirname $BASH_SOURCE[0]);  # cd current directory
+set -x
+
+wat2wasm checkers.wat
+wasm2wat checkers.wasm -o checkers.wasm.wat
+wasm2c   checkers.wasm -o checkers.wasm.c
+wasm-objdump checkers.wasm -hdxrs | tee checkers.wasm.objdump
+
+## WABT Tools: https://github.com/WebAssembly/wabt
+wat-desugar    checkers.wat      # strip comments + flat format syntax)
+file           checkers.wasm     # checkers.wasm: WebAssembly (wasm) binary module version 0x1 (MVP)
+hexdump        checkers.wasm     # 0000000   6100 6d73 0001 0000
+wasm-decompile checkers.wasm     # transpile to C (pseudocode)
+wasm-validate  checkers.wasm -v  # BeginExportSection EndExportSection
+
+ls -l *.wat *.wasm* | sort -nr -k5
