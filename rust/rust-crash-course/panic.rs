551ec5174da0377525a56be5696d05aa1eaa468f
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-07-03 05:10:13 +0100

    rust-crash-course | panic.rs | document RUST_BACKTRACE=1

--- rust/rust-crash-course/panic.rs
+++ rust/rust-crash-course/panic.rs
@@ -1,3 +1,24 @@
+// DOCS: https://www.youtube.com/watch?v=wM6o70NAWUI&ab_channel=Let%27sGetRusty
+// $ rustc panic.rs; RUST_BACKTRACE=1 ./panic
+// thread 'main' panicked at 'Hello World!', panic.rs:23:5
+// stack backtrace:
+// 0: std::panicking::begin_panic
+// 1: panic::c
+// 2: panic::b
+// 3: panic::a
+// 4: panic::main
+// 5: core::ops::function::FnOnce::call_once
+// note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
+
 fn main() {
+    a();
+}
+fn a() {
+    b();
+}
+fn b() {
+    c();
+}
+fn c() {
     panic!("Hello World!");
 }
