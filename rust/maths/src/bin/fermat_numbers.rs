e54ee472a550a402fc418640a169b5a76a4bb4e4
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-08-05 10:15:12 +0100

    Rust | Fermat Numbers | refactor using GMP rug

--- rust/maths/src/bin/fermat_numbers.rs
+++ rust/maths/src/bin/fermat_numbers.rs
@@ -1,39 +1,46 @@
 use std::time::Instant;
+use rug::{Integer, ops::Pow};
 
 fn main() {
-    for n in 0..20 {
+    for n in 0..21 {
         let time_start = Instant::now();
-        let value = fermat(n);
-        let time_taken = time_start.elapsed().as_micros();
-        println!("{time_taken:5}us | fermat({n}) = {value}");
+        let value = fermat_number(n);
+        let is_prime= value.is_probably_prime(128);
+        // let value = Float::with_val(128, value);  // BUG: https://gitlab.com/tspiteri/rug/-/issues/42
+        let time_taken = time_start.elapsed();
+        println!("{time_taken:?} | fermat({n}) = {is_prime:?} {value}");
     }
 }
 
-fn fermat(n: u32) -> u128 {
-    u128::pow(2, u32::pow(2, n)) + 1
+fn fermat_number(n: u32) -> Integer {
+    Integer::from(2).pow( u32::pow(2,n) ) + 1
+    // u128::pow(2, u32::pow(2, n)) + 1
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // DOCS: https://en.wikipedia.org/wiki/Fermat_number
     #[test]
     fn fermat_numbers() {
-        let a000215: Vec<u128> = vec![
-            3,
-            5,
-            17,
-            257,
-            65537,
-            4294967297,
-            18446744073709551617,
-            // 340282366920938463463374607431768211457,
-            // 115792089237316195423570985008687907853269984665640564039457584007913129639937,
-        ];
+        // DOCS: https://en.wikipedia.org/wiki/Fermat_number
+        let a000215 = [
+            "3",
+            "5",
+            "17",
+            "257",
+            "65537",
+            "4294967297",
+            "18446744073709551617",
+            "340282366920938463463374607431768211457",
+            "115792089237316195423570985008687907853269984665640564039457584007913129639937",
+        ]
+            .into_iter()
+            .map(|n| Integer::from_str_radix(n, 10).unwrap())
+            .collect::<Vec<_>>()
+        ;
         for (n, value) in a000215.into_iter().enumerate() {
-            assert_eq!(fermat(n as u32), value, "fermat({n})");
+            assert_eq!(fermat_number(n as u32), value, "fermat({n})");
         }
     }
 }
