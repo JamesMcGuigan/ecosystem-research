baadab58711d88014152eefe576edfe1aa9796b0
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-08-23 07:59:21 +0100

    ICP | greeter | implement counter

--- icp/greeter/src/greeter_backend/src/lib.rs
+++ icp/greeter/src/greeter_backend/src/lib.rs
@@ -1,4 +1,18 @@
-#[ic_cdk_macros::query]
+#![feature(local_key_cell_methods)]
+
+use std::cell::RefCell;
+
+thread_local! {
+    static COUNT: RefCell<u64> = RefCell::new(0);
+}
+
+#[ic_cdk_macros::update]
 fn greet(name: String) -> String {
+    COUNT.with(|count| *count.borrow_mut() += 1);
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk_macros::query]
+fn get_count() -> u64 {
+    COUNT.with(|count| count.borrow().clone())
 }
--- icp/greeter/src/greeter_frontend/src/index.js
+++ icp/greeter/src/greeter_frontend/src/index.js
@@ -3,17 +3,21 @@ import { greeter_backend } from "../../declarations/greeter_backend";
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
   button.setAttribute("disabled", true);
 
   // Interact with foo actor, calling the greet method
-  const greeting = await greeter_backend.greet(name);
+  const name = document.getElementById("name").value.toString();
+
+  // Avoid race condition by calling sequentially
+  document.getElementById("greeting").innerText = await greeter_backend.greet(name);
+  document.getElementById("count").innerText    = String(await greeter_backend.get_count());
 
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
   return false;
 });
+
+window.onload = function() {
+  greeter_backend.get_count().then(count => {
+    document.getElementById("count").innerText = count;
+  })
+}
