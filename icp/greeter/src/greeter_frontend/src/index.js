8b5b0ec267cd561289a19007c9eadc9bc9c8f974
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-08-23 06:45:47 +0100

    ICP | dfx new greeter --type rust

---
+++ icp/greeter/src/greeter_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { greeter_backend } from "../../declarations/greeter_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await greeter_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
